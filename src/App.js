4e10620a05e18cf60f7d1b94abc3ec8b4d64954f
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-01-16 17:50:02 -0300

    repositorio do curso react para iniciantes da udemy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './index.css';
+import {v4 as uuidv4 } from 'uuid'
+import NavBar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskLisk/TaskList";
+
+const generateId = () => {
+  return uuidv4();
+}
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTaskApp = (title, state) => {
+    console.log('funcao sendo chamada aqui em App');
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      console.log(existingTasks)
+      console.log(newTask)
+      return [...existingTasks, newTask];   
+    })
+  };
+
+  const updateTask = (id, title, state) => {
+    console.log('chamando sim update task')
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if(task.id === id){
+          return {...task, title, state};
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter(task => task.id !== id);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <div className="container">
+        <TaskList 
+          title="To Do" 
+          onAddTask={addTaskApp} 
+          tasks={tasks.filter((t) => t.state === "To Do")} 
+          onTaskUpdate={updateTask}
+          taskState="To Do"
+          onDeleteTask={deleteTask}
+        />
+        <TaskList 
+          title="Doing"
+          onAddTask={addTaskApp} 
+          tasks={tasks.filter((t) => t.state === "Doing")} 
+          onTaskUpdate={updateTask}
+          taskState="Doing"
+          onDeleteTask={deleteTask}
+        />
+        <TaskList 
+          title="Done"
+          onAddTask={addTaskApp} 
+          tasks={tasks.filter((t) => t.state === "Done")} 
+          onTaskUpdate={updateTask}
+          taskState="Done"
+          onDeleteTask={deleteTask}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/TaskItem/TaskItem.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import './TaskItem.css'
+
+export default function TaskItem({id, title, taskState, onTaskUpdate, onDeleteTask}){
+    const [isEditing, setIsEditing] = useState(false);
+    const [editableTitle, setEditableTitle] = useState(title);
+
+    const onTitleChange = (event) => {
+        const newTitle = event.target.value;
+        setEditableTitle(newTitle);
+        onTaskUpdate(id, newTitle, taskState)
+    }
+
+    const onKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            setIsEditing(false);
+            if(editableTitle.length === 0){
+                onDeleteTask(id);
+            }
+        }
+    }
+
+    const onTaskStateChange = (event) => {
+        onTaskUpdate(id, title, event.target.value)
+    }
+
+    if(isEditing){
+        return(
+            <div className="task-item">
+                <input type="text" value={editableTitle} onChange={onTitleChange} onKeyPress={onKeyPress}></input>
+            </div>
+        );
+    } else {
+        return(
+            <div className="task-item">
+                <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>
+                <select onChange={onTaskStateChange} value={taskState}>
+                    <option value="To Do">To Do</option>
+                    <option value="Doing">Doing</option>
+                    <option value="Done">Done</option>
+                </select>
+            </div>
+        );
+    }
+}
+
+TaskItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    taskState: PropTypes.string.isRequired
+};
